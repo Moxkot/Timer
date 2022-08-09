d7327884f27b0405b3fc6f42292a351237be2645
Шумилин Денис <moxkot@yandex.ru>
2022-08-09 22:55:34 +0300

    task 9.1 has been done

--- index.js
+++ index.js
@@ -8,13 +8,35 @@
 - запускается сам таймер, значения которого берутся из ранее сохранненной переменной;
 4. на кнопку "Сбросить" добавляется обработчик, который делает все обратное (возвращает исходное состояние тацмера, удаляет класс hide везед где добавляли его);
 5. Делаем так чтобы при обновлении страницы таймер не соскакивал.
-
-
-
-
-
-
-
-
-
 */
+
+'use strict'
+
+let titleDate = document.querySelector('#title-date');
+let date = document.querySelector('#date');
+let btnStart = document.querySelector('#btn');
+let btnReset = document.querySelector('#btn-reset');
+let nameTimer = document.querySelector('h1');
+let startData = document.querySelector('.input');
+let timerData = document.querySelector('.output');
+
+function countdown() {
+  console.log(titleDate.value);
+  console.log(date.value); 
+  nameTimer.textContent = titleDate.value;
+  let countdownDate = date.value;
+  if(!countdownDate) {
+    alert('Пожалуйста, введите дату');
+  } else {
+    return startTimer;
+  };
+};
+
+function startTimer() {
+  btnStart.classList.add('hide');
+  btnReset.classList.remove('hide');
+  startData.classList.add('hide');
+  timerData.classList.remove('hide');
+}
+
+btnStart.addEventListener('click', countdown);
