dd5d974ab7722c625ae940bcb1b06b2514b7070d
Шумилин Денис <moxkot@yandex.ru>
2022-08-23 23:23:31 +0300

    add 10.2

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-/*
-1. Сохраняем в переменную название заголовка таймера.
-2. Выбираем дату отсчета и также сохраняем в переменную.
-3. Добавляем обработчик на кнопку начать. При выполенеии обработчика запускается функция, которая
-- добавляет класс hide к div.input;
-- в h1 записываем содержимое переменной заголовка таймера;
-- кнопку "Начать" также скрываем;
-- запускается сам таймер, значения которого берутся из ранее сохранненной переменной;
-4. на кнопку "Сбросить" добавляется обработчик, который делает все обратное (возвращает исходное состояние тацмера, удаляет класс hide везед где добавляли его);
-5. Делаем так чтобы при обновлении страницы таймер не соскакивал.
-*/
-
 'use strict'
 
 let titleDate = document.querySelector('#title-date');
-let date = document.querySelector('#date');
+let dateTimer = document.querySelector('#date');
 let btnStart = document.querySelector('#btn');
 let btnReset = document.querySelector('#btn-reset');
 let nameTimer = document.querySelector('h1');
 let startData = document.querySelector('.input');
 let timerData = document.querySelector('.output');
+let h2 = document.querySelector('.complete');
+let countdownDate;
+let diff;
+let numbers = document.querySelector('.numbers');
+
 
 function countdown() {
-  console.log(titleDate.value);
-  console.log(date.value); 
-  nameTimer.textContent = titleDate.value;
-  let countdownDate = date.value;
+  countdownDate = new Date(dateTimer.value);
+  let dateNow = Date.now();
+  diff = countdownDate - dateNow;
+  nameTimer.textContent = titleDate.value;  
+  
   if(!countdownDate) {
     alert('Пожалуйста, введите дату');
   } else {
-    return startTimer;
+    return startTimer();
   };
 };
+
+
+function checkDate() {
+  if(diff <= 0) {
+    timerData.classList.add('hide');
+    h2.classList.remove('hide');
+    h2.textContent = `${titleDate.value} завершился ${dateTimer.value}`;
+    return;
+  };
+}
 
 function startTimer() {
   btnStart.classList.add('hide');
   btnReset.classList.remove('hide');
   startData.classList.add('hide');
   timerData.classList.remove('hide');
-}
+
+  checkDate();
+
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff/(1000*60*60))%24);
+  let minutes = Math.floor((diff/(1000*60))%60);
+  let seconds = Math.floor((diff/1000)%60);
+  numbers.textContent = `${days}:${hours}:${minutes}:${seconds}`; 
+};
 
 btnStart.addEventListener('click', countdown);
